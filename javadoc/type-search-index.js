62cf7ca115df6c1cd0cdb9d76fe318ad74de1d00
MontseCaballero29 <151937757+MontseCaballero29@users.noreply.github.com>
2025-04-11 19:31:41 -0600

    Java docs

---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"libreria","l":"Convertir"},{"p":"usolibreriaconversiones","l":"JFrame"}];updateSearchResults();
